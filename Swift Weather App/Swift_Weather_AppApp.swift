1a2359abac0da6b364ddbce3f25d0bbeb69a0ce7
Jay Calderon <cald0145@algonquinlive.com>
2024-11-21 08:55:58 -0500

    Initial Commit

---
+++ Swift Weather App/Swift_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Weather_AppApp.swift
+//  Swift Weather App
+//
+//  Created by Jay Calderon on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift Weather AppTests/Swift_Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Weather_AppTests.swift
+//  Swift Weather AppTests
+//
+//  Created by Jay Calderon on 2024-11-21.
+//
+
+import Testing
+@testable import Swift_Weather_App
+
+struct Swift_Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
